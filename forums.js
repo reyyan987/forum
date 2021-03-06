7593864afd995c68984c19a2f44c3bbedec16525
reyyan987 <67619958+reyyan987@users.noreply.github.com>
2021-03-06 15:22:51 +0500

    Add files via upload

---
+++ forums.js
@@ -0,0 +1,29 @@
+ 
+function myFunction() {
+  document.getElementById("uull").style.left = "-100%";
+  document.getElementById("hale").style.visibility = "visible";
+  document.getElementById("hela").style.visibility = "hidden";
+ 
+}
+function myNot() {
+ document.getElementById("uull").style.left = "-5%";
+ document.getElementById("hale").style.visibility = "hidden";
+ document.getElementById("hela").style.visibility = "visible";
+}
+function myHade() {
+ document.getElementById("uull").style.left = "-100%";
+ document.getElementById("hale").style.visibility = "visible";
+ document.getElementById("hela").style.visibility = "hidden";
+}
+function Youtube() {
+window.location.assign("https://www.youtube.com")
+
+}
+function facebook() {
+window.location.assign("https://www.google.com")
+
+}
+function discord() {
+window.location.assign("https://www.google.com")
+
+}
